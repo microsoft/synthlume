dd69f04edd911bf85d8bdbbb04cfc89a304a442a
Dimitrios Mavroeidis <dmavroeidis@microsoft.com>
2024-06-07 10:39:36 +0200

    first end-to-end working version of cluster-based sampling

--- synthlume/pipeline/utils/sampler.py
+++ synthlume/pipeline/utils/sampler.py
@@ -1,29 +1,38 @@
-import os
-from copy import deepcopy
-
 from langchain_core.documents import Document
-from abc import ABC
+from abc import ABC, abstractmethod
 from synthlume.logging.logging import get_logger
+from typing import Any
 
 logger = get_logger(__name__)
 
-class ChunkSampling(ABC):
-    """Chunk sampling for document chunks interface
+class Sampler(ABC):
+    """Chunk sampling interface. Chunk sampler relies on document clustering to group similar documents together. 
+    The sampling strategy is implemented by the subclass.
     
     Parameters:
     - documents: list of input document chunks 
-    - sample_size: number of chunks to be sampled
     """
     name: str = "generic sampler"
 
     def __init__(
         self,
         documents: list[Document],
-        sample_size: int, #number of chunks to be sampled
+        **kwargs: Any,
     ):
-        self.sample_size = sample_size
-        self.documents = documents[:sample_size]
+        self.documents = [documents] #Each nested list represents a cluster of document chunks. Initializaing with a single cluster
+    
+    @abstractmethod
+    def _clustering(
+        self, 
+        **kwargs: Any,
+    ) -> list[int]:
+        """Cluster documents into chunks
+        
+        Returns:
+        - list of cluster indices
+        """
 
+    @abstractmethod
     def __iter__(self):
-        for i in range(len(self.documents)):
-            yield self.documents[i]
+        """Iterator that yields the next chunk of documents based on the implemented sampling strategy"""
+        
